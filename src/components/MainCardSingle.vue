5a31fb973e88e369fdd96571a3693351c18aaee8
Jeremyah Botello <jbotel17@stu.southtexascollege.edu>
2025-02-26 23:15:42 -0600

    initialized with MainCardSingle

---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { faker } from '@faker-js/faker'
+
+const recipeName = faker.food.dish();
+const description = faker.lorem.sentences(2);
+const imageUrl = faker.image.urlLoremFlickr({ category: 'food', height: 480, width: 960 });
+const recipeId = faker.string.uuid();
+</script>
+
+<template>
+  <RouterLink :to="{ name: 'CardDetail', params: { id: recipeId }}">
+    <div class="border border-gray-500 rounded-lg shadow bg-gray-400 h-full">
+      <img class="object-fill h-480 w-960 rounded-t-lg" :src="imageUrl" />
+      <div class="p-4">
+        <p class="mb-2 text-2xl font-bold">{{ recipeName }}</p>
+        <p class="mb-3 font-normal text-gray-800">{{ description }}</p>
+      </div>
+    </div>
+  </RouterLink>
+</template>
