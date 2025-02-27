415bbe5de52b6254f4a8925eda8c222c6d8486a0
Jeremyah Botello <jbotel17@stu.southtexascollege.edu>
2025-02-26 23:50:00 -0600

    added my composables and set up firebase

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgEko6S1z6e4liJsHe9_OB7x2CMLRpXNc",
+  authDomain: "project5-51114.firebaseapp.com",
+  projectId: "project5-51114",
+  storageBucket: "project5-51114.firebasestorage.app",
+  messagingSenderId: "370653971874",
+  appId: "1:370653971874:web:d437145c2eb9a9a722defb"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
--- src/router.js
+++ src/router.js
@@ -1,15 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
 import HomePage from '@/views/HomePage.vue'
+import CardDetail from './views/CardDetail.vue'
+import LoginPage from './views/LoginPage.vue'
+import SettingsPage from './views/SettingsPage.vue'
+import { useAuth } from './composables/useAuth'
+
+const { isAuthenticated } = useAuth()
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/project5/', name: 'Home', component: HomePage },
+  { path: '/project5/recipes/:id', name: 'CardDetail', component: CardDetail },
+  { path: '/project5/login', name: 'LoginPage', component: LoginPage },
+  { path: '/project5/settings', name: 'SettingsPage', meta: { requiresAuth: true }, component: SettingsPage },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+// Navigation guard
+router.beforeEach((to, _, next) => {
+  if (to.meta.requiresAuth && !isAuthenticated.value) {
+    next({ name: 'LoginPage', query: { redirect: to.fullPath } })
+  } else {
+    next()
+  }
 })
 
 export default router
